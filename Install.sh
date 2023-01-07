ff1ab8e3cacbcc0815987adc253c2f8bebde93bc
G. Ribeiro <34517016+BRGustavoRibeiro@users.noreply.github.com>
2023-01-07 10:21:22 -0300

    code: first commit

---
+++ Install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+echo "\n$(tput setab 11)                                                           " 
+echo "$(tput setab 11) $(tput setab 4)      $(tput setab 11)                                                    "
+echo "$(tput setab 11) $(tput setab 4)      $(tput setaf 0)$(tput setab 11)   App Itaú no Computador para Linux [NÃO-OFICIAL]  "
+echo "$(tput setab 11) $(tput setaf 7)$(tput setab 4) Itaú $(tput setab 11)                                                    "
+echo "$(tput setab 11)                                                           " 
+tput sgr0
+echo "\nVersão 2023.01.001\n"
+echo "Informações sobre este script e declaração de ciência"
+echo "-------------\n"
+echo "1. Este software e script foram criados para oferecer uma maneira de instalar o App Itaú no Computador dentro de ambientes Linux, facilitando a instalação de dependências necessárias para sua execução. Este script é oferecido de forma pública e gratuita, e o código é aberto e disponível para sugestões, melhorias e modificações.\n"
+echo "2. Este script não é criado, fornecido, patrocinado ou auditado pelo Banco Itaú ou qualquer uma de suas subsidiárias de nenhuma maneira.\n"
+echo "3. Este script é fornecido 'como é'/'como está', e não possui garantia de funcionamento pleno. Nenhuma pessoa envolvida com a criação deste script não é responsável por absolutamente nenhuma ocorrência ou dano causado no seu computador, nem é responsável por nenhum tipo de ocorrência ou problema na sua conta bancária.\n"
+echo "4. Este software não fornece o binário do App Itaú no Computador e apenas baixa automaticamente o executável disponível publicamente no site https://guardiao.itau.com.br/UpdateServer/aplicativoitau.msi e executa as operações técnicas necessárias para assegurar seu funcionamento fora do ambiente Windows, e quaisquer dúvidas sobre suas funcionalidades devem ser redirecionadas ao Itaú Unibanco.\n"
+echo "5. Este script, por si, não envia nenhum tipo de informação para seu criador nem para nenhum terceiro e não recolhe dados de nenhuma natureza.\n"
+echo "6. O criador deste script, o Banco Itaú e qualquer outro colaborador deste projeto não tem nenhuma responsabilidade por manter e assegurar o pleno funcionamento deste software ou do projeto. Este script também pode parar de funcionar a qualquer momento e poderá eventualmente ser removido sem aviso prévio da disponibilização pública.\n"
+
+echo "- Eventuais dúvidas sobre o funcionamento deste script podem ser resolvidas com o encaminhamento de uma issue em https://github.com/BRGustavoRibeiro/app-itau-para-linux\n"
+tput sgr0
+echo "AO UTILIZAR ESTE SCRIPT, VOCÊ CONCORDA INTEGRALMENTE COM OS TERMOS ACIMA, E DECLARA TER TOTAL E PLENA CIÊNCIA DAS INFORMAÇÕES FORNECIDAS."
+read -r -p "DESEJA CONTINUAR? (S/N): " response
+case "$response" in
+    [sS][iI][mM]|[sS]) 
+        echo "\n\n$(tput setaf 0)$(tput setab 11) -- INICIANDO INSTALAÇÃO -- $(tput sgr0)\nIsto pode levar alguns minutos.\n\n"
+        ;;
+    *)
+        echo "O software não será instalado."
+        exit 0
+        ;;
+esac
+
+echo "[LOG] Atualizando repositórios"
+sudo add-apt-repository -y ppa:flatpak/stable
+sudo apt-get update
+
+if [ $(which flatpak) ]
+then 
+  echo "[LOG] Flatpak instalado!"
+else
+  echo "[LOG] Flatpak não detectado - instalando"
+  sudo apt install -y flatpak
+fi
+
+if [ $(which bottles) ]
+then 
+  echo "[LOG] Flatpak instalado!"
+else
+  echo "[LOG] Flatpak não detectado - instalando"
+  sudo apt install -y flatpak
+  echo "[LOG] Instalando Bottles"
+  sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  sudo flatpak install flathub com.usebottles.bottles -y
+fi
+
+echo "[LOG] Baixando Aplicativo Itaú"
+wget -O winsetup.msi https://guardiao.itau.com.br/UpdateServer/aplicativoitau.msi
+
+echo "[LOG] Configurando ambiente"
+flatpak run --command=bottles-cli com.usebottles.bottles new --bottle-name ItauLinux --environment application --arch win64
+mv winsetup.msi $(eval echo ~)/.var/app/com.usebottles.bottles/.local/winsetup.msi
+echo "[LOG] Instalando Aplicativo Itaú"
+flatpak run --command=bottles-cli com.usebottles.bottles run -b ItauLinux -e $(eval echo ~)/.var/app/com.usebottles.bottles/.local/winsetup.msi --args /quiet
+echo "[LOG] Criando permissão para criar atalho"
+flatpak override com.usebottles.bottles --user --filesystem=xdg-data/applications
+echo "[LOG] Criando atalho na Área de Trabalho e no menu"
+wget -O $(eval echo ~)/.local/share/applications/appitaulinux.desktop https://raw.githubusercontent.com/BRGustavoRibeiro/app-itau-para-linux/main/resources/appitaulinux.desktop
+sed -i "s#DIRETORIO#$(eval echo ~)" $(eval echo ~)/.local/share/applications/appitaulinux.desktop
+echo "\n\n$(tput setaf 0)$(tput setab 11) Instalação concluída! $(tput sgr0)"
+exit 0
